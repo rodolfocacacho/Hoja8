f57d3ab041455c51bc3aeba89608ab07cf374766
Rodolfo Cacacho <RodolfoC@192.168.1.2>
2016-10-02 11:39:52 -0600

    Se agrega la clase paciente.

---
+++ Hoja8/src/Paciente.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RodolfoC
+ */
+public class Paciente {
+    
+    private String nombre;
+    private String descripcion;
+    private String prioridad;
+
+    public Paciente(String nombre, String descripcion, String prioridad) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+    }
+    
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+    
+
+    
+}
