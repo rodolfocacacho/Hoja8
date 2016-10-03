9dbe8ea5211098987d215693840155b9f6e704d1
Rodolfo Cacacho <RodolfoC@192.168.1.2>
2016-10-02 18:06:17 -0600

    Main actualizado.

---
+++ Hoja8/src/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+
+public class Main {
+
+public static void main(String[] args){
+	
+	System.out.println("Bienvenido al programa\n");
+	//Leer archivo de texto
+	String fileName = "pacientes.txt";
+	BufferedReader in = new BufferedReader(new FileReader(fileName));
+	//Se crea para el split
+	String [] subStrings;
+	//Vector que contendra todos los objetos pacientes
+	Vector<Paciente> VecPacientes = new Vector<Paciente>();
+	try {
+		//Mientras haya una linea de paciente continua leyendo el archivo
+		while ((linea = in.readLine()) != null) {
+			subStrings = linea.split(",");
+			//la linea esta ahora divida en 3 [nombre, sintomas, codigo]
+			VecPacientes.add(new Paciente(subStrings[0], subStrings[1], subStrings[2]));
+			//Se añade el objeto paciente al vector que los contendra a todos
+		}			
+	} catch (IOException e) {
+			//Error si hay algun problema con el ingreso de datos
+			System.out.println("Ha ocurrido un error");
+	}
+}
+}
